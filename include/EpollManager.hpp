924bca95ff78b2218cf100f38ef30534b8d35682
Dendrog <goodshs335@gmail.com>
2025-06-29 02:09:35 +0900

    feat: Epoll 관리함수 추가 및 Server전달용 getter 추가

--- include/EpollManager.hpp
+++ include/EpollManager.hpp
@@ -11,24 +11,47 @@
 class EpollManager {
 	private:
 		int _epollFd;
-		const int _EPOLL_SIZE = 64;
+		static const int	EPOLL_SIZE = 64;
 		std::vector<struct epoll_event> _epollEvents;
 		struct epoll_event _event;
 		int	_eventCount;
 
 	public:
 		void	initEpoll(int serverSocket) {
-			_epollFd = epoll_create(_EPOLL_SIZE);
-			_epollEvents.resize(_EPOLL_SIZE);
+			_epollFd = epoll_create(EPOLL_SIZE);
+			_epollEvents.resize(EPOLL_SIZE);
+			addEpollFd(serverSocket);
+		}
+
+		void	addEpollFd(int socketFd) {
 			_event.events = EPOLLIN;
-			_event.data.fd = serverSocket;
-			epoll_ctl(_epollFd, EPOLL_CTL_ADD, serverSocket, &_event);
-			
+			_event.data.fd = socketFd;
+			epoll_ctl(_epollFd, EPOLL_CTL_ADD, socketFd, &_event);
+		}
+
+		void	deleteEpollFd(int socketFd) {
+			epoll_ctl(_epollFd, EPOLL_CTL_DEL, socketFd, NULL);
+			close(socketFd);
 		}
 
 		void	waitEvent() {
-			if (_eventCount = (epoll_wait(_epollFd, (struct epoll_event *)_epollEvents.data(), _EPOLL_SIZE, -1)) == -1)
+			if ((_eventCount = (epoll_wait(_epollFd, (struct epoll_event *)_epollEvents.data(), EPOLL_SIZE, -1))) == -1)
 				std::cout << "error" << std::endl; // throw 변경
+		}
+
+		int	getEpollFd() {
+			return _epollFd;
+		}
+
+		int	getEventCount() {
+			return _eventCount;
+		}
+
+		int	getEpollEventsFdAt(int index) {
+			if (index < 0 || static_cast<int>(_epollEvents.size()) <= index) {
+				std::cout << "error" << std::endl; // throw 변경
+			}
+			return _epollEvents[index].data.fd;
 		}
 };
 
