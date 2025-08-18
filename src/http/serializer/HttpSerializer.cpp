d6d823fe50efec66209f6a25f0a09238b95b91b2
iamdudumon <kksy4511@naver.com>
2025-08-18 16:11:20 +0900

    feat: HttpSerializer class 구현
    - HttpPacket(Request/Response) 객체를 std::string type으로 직렬화

---
+++ src/http/serializer/HttpSerializer.cpp
@@ -0,0 +1,40 @@
+// HttpSerializer.cpp
+#include "HttpSerializer.hpp"
+
+#include <sstream>
+
+#include "../../utils/str_utils.hpp"
+
+std::string HttpSerializer::serialize(const HttpPacket& packet) {
+	std::stringstream						  ss;
+	const std::map<std::string, std::string>& headers =
+		packet.getHeader().getHeaders();
+	const std::vector<unsigned char>& bodyData = packet.getBody().getData();
+
+	if (packet.isRequest()) {
+		const HTTP::StartLine& startLine = packet.getStartLine();
+
+		ss << HTTP::Method::to_string(startLine.method) << " "
+		   << startLine.target << " " << startLine.version << "\r\n";
+	} else {
+		const HTTP::StatusLine& statusLine = packet.getStatusLine();
+
+		ss << statusLine.version << " "
+		   << HTTP::StatusCode::to_string(statusLine.statusCode) << " "
+		   << statusLine.reasonPhrase << "\r\n";
+	}
+
+	for (std::map<std::string, std::string>::const_iterator it =
+			 headers.begin();
+		 it != headers.end(); ++it) {
+		ss << it->first << ": " << it->second << "\r\n";
+	}
+
+	ss << "\r\n";
+
+	if (!bodyData.empty()) {
+		ss.write(reinterpret_cast<const char*>(&bodyData[0]), bodyData.size());
+	}
+
+	return ss.str();
+}
