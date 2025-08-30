eaffa9c7830a0786f5a537d7ab013deba0b8b136
iamdudumon <kksy4511@naver.com>
2025-08-30 23:35:36 +0900

    feat: 응답 전용 직렬화 및 헤더 보강
    - Requset 객체 직렬화 차단(예외 발생) -> Response 객체 전용 사용 보장
    - Content-Length를 실제 바디 길이로 강제 출력
    - 응답에 Server 헤더 자동 추가

--- src/http/serializer/HttpSerializer.cpp
+++ src/http/serializer/HttpSerializer.cpp
@@ -2,39 +2,43 @@
 #include "HttpSerializer.hpp"
 
 #include <sstream>
+#include <stdexcept>
 
 #include "../../utils/str_utils.hpp"
 
 std::string HttpSerializer::serialize(const HttpPacket& packet) {
+	if (packet.isRequest())
+		throw std::logic_error("HttpSerializer: request packet unsupported");
+
 	std::stringstream						  ss;
 	const std::map<std::string, std::string>& headers =
 		packet.getHeader().getHeaders();
 	const std::vector<unsigned char>& bodyData = packet.getBody().getData();
+	const size_t					  bodyLen = bodyData.size();
+	const HTTP::StatusLine			  statusLine = packet.getStatusLine();
+	bool							  hasServer = false;
+	bool							  hasContentLength = false;
 
-	if (packet.isRequest()) {
-		const HTTP::StartLine& startLine = packet.getStartLine();
-
-		ss << HTTP::Method::to_string(startLine.method) << " "
-		   << startLine.target << " " << startLine.version << "\r\n";
-	} else {
-		const HTTP::StatusLine& statusLine = packet.getStatusLine();
-
-		ss << statusLine.version << " "
-		   << HTTP::StatusCode::to_string(statusLine.statusCode) << " "
-		   << statusLine.reasonPhrase << "\r\n";
-	}
-
+	ss << statusLine.version << " "
+	   << HTTP::StatusCode::to_string(statusLine.statusCode) << " "
+	   << statusLine.reasonPhrase << "\r\n";
 	for (std::map<std::string, std::string>::const_iterator it =
 			 headers.begin();
 		 it != headers.end(); ++it) {
+		std::string keyLower = to_lower(it->first);
+		if (keyLower == "content-length") {
+			hasContentLength = true;
+			continue;
+		}
+		if (keyLower == "server") hasServer = true;
 		ss << it->first << ": " << it->second << "\r\n";
 	}
-
+	if (!hasServer) ss << "Server: webserv" << "\r\n";
+	if (bodyLen > 0 || hasContentLength)
+		ss << "Content-Length: " << bodyLen << "\r\n";
 	ss << "\r\n";
-
-	if (!bodyData.empty()) {
+	if (!bodyData.empty())
 		ss.write(reinterpret_cast<const char*>(&bodyData[0]), bodyData.size());
-	}
 
 	return ss.str();
 }
