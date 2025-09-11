d77c6e6390fa6ded81ea090bc8efb11e1e0a984a
jinseong97 <jinseong1121@gmail.com>
2025-09-11 12:54:07 +0900

    refactor: validate 함수 분리

--- src/config/ConfigValidator.hpp
+++ src/config/ConfigValidator.hpp
@@ -10,6 +10,9 @@
 
 class ConfigValidator {
 	private:
+		static void validatePort(const Config&, std::set<int>&);
+		static void validateLocations(const Config&);
+		static void validateLocation(const std::string&, const ConfigLocation&);
 
 	public:
 		static void validate(const std::vector<Config>& configs);
