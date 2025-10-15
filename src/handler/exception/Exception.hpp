161cbd5492627fb054e840ef577e5e66e87c4fd1
iamdudumon <kksy4511@naver.com>
2025-10-15 17:13:43 +0900

    hotfix: explicit 키워드 추가

--- src/handler/exception/Exception.hpp
+++ src/handler/exception/Exception.hpp
@@ -12,7 +12,7 @@ namespace handler {
 
 		public:
 			Exception() {}
-			Exception(const std::string& message) : _message(message) {}
+			explicit Exception(const std::string& message) : _message(message) {}
 			virtual ~Exception() throw() {}
 
 			virtual const char* what() const throw() {
